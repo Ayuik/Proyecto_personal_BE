441e4d5f60670d887e36185578a976b6207cde36
Ayuik <marcosayelen@gmail.com>
2025-04-11 14:28:29 +0200

    first commit

---
+++ src/main/java/dev/ayelen/ProyectoPersonaBeApplication.java
@@ -0,0 +1,13 @@
+package dev.ayelen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPersonaBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPersonaBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ayelen/ProyectoPersonaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayelen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPersonaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
