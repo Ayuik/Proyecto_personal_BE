a6c66a430d673cd8616f4f9fdd38c8e2b7451715
Ayuik <marcosayelen@gmail.com>
2025-04-12 10:12:45 +0200

    feat: CourseController, Service and Repository created

--- src/main/java/dev/ayelen/courses/CourseService.java
+++ src/main/java/dev/ayelen/courses/CourseService.java
@@ -1,8 +1,35 @@
 package dev.ayelen.courses;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class CourseService {
+    CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public Course store(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Course update(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Course getById(Long id) {        
+        return courseRepository.findById(id).orElse(null);    
+    }
+
+    public void delete(Long id) {
+        courseRepository.deleteById(id);    
+    }   
+
+    public List<Course> getAll() {
+        return courseRepository.findAll();    
+    }
     
 }
