39f597d00718669f271ad1e31d53b44825865ac8
Ayuik <marcosayelen@gmail.com>
2025-04-12 08:58:15 +0200

    feat: Course and Category entities created and related

---
+++ src/main/java/dev/ayelen/courses/Category.java
@@ -0,0 +1,30 @@
+package dev.ayelen.courses;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="categories")
+public class Category {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_category", nullable = false)
+    private Long categoryId;
+    
+    @Column(name = "category_name", nullable = false)
+    private String categoryName;
+
+    public Category() {}
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;    
+    }
+    
+}
