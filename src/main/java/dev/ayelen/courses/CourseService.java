52f3de5d6b29139ccda9911090febf0ccffd0aa7
Ayuik <marcosayelen@gmail.com>
2025-04-12 12:18:26 +0200

    feat: setters added for Course attributes

--- src/main/java/dev/ayelen/courses/CourseService.java
+++ src/main/java/dev/ayelen/courses/CourseService.java
@@ -2,7 +2,9 @@ package dev.ayelen.courses;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CourseService {
@@ -16,8 +18,19 @@ public class CourseService {
         return courseRepository.save(course);
     }
 
-    public Course update(Long courseId, Course course) {
-        return courseRepository.save(course);
+    public Course update(Long courseId, Course updatedCourseData) {
+        Course existingCourse = courseRepository.findById(courseId)
+        .orElseThrow(() -> new ResponseStatusException(
+            HttpStatus.NOT_FOUND, "Course not found with id: " + courseId));
+        existingCourse.setCouseTitle(updatedCourseData.getCourseTitle());
+        existingCourse.setCourseCategory(updatedCourseData.getCourseCategory());
+        existingCourse.setCourseDescription(updatedCourseData.getCourseDescription());
+        existingCourse.setCourseNumVideos(updatedCourseData.getCourseNumVideos());
+        existingCourse.setCourseDuration(updatedCourseData.getCourseDuration());
+        existingCourse.setCourseCover(updatedCourseData.getCourseCover());
+        existingCourse.setCoursePrice(updatedCourseData.getCoursePrice());
+
+        return courseRepository.save(existingCourse);
     }
 
     public Course getById(Long id) {        
