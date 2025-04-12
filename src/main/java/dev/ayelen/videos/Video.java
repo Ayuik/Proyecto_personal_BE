ab3a3baf5c5b6219cafc5b2fad3a47f68e483226
Ayuik <marcosayelen@gmail.com>
2025-04-12 17:06:53 +0200

    feat: Video entity created

---
+++ src/main/java/dev/ayelen/videos/Video.java
@@ -0,0 +1,90 @@
+package dev.ayelen.videos;
+
+import dev.ayelen.courses.Course;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "videos")
+public class Video {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_video", nullable = false)
+    private Long videoId;
+
+    @Column(name = "video_title", nullable = false)
+    private String videoTitle;
+
+    @Column(name = "video_url", nullable = false)
+    private String videoUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "video_course_id", referencedColumnName = "id_course", nullable = false)
+    private Course course;
+
+    @Column(name = "video_description", nullable = false)
+    private String videoDescription;
+
+    @Column(name = "video_duration", nullable = false)
+    private int videoDuration;
+
+    public Video() {}
+
+    public Video(String videoTitle, String videoUrl, Course course, String videoDescription, int videoDuration) {
+        this.videoTitle = videoTitle;
+        this.videoUrl = videoUrl;
+        this.course = course;
+        this.videoDescription = videoDescription;
+        this.videoDuration = videoDuration;
+    }
+
+    public Long getVideoId() {
+        return videoId;
+    }
+
+    public String getVideoTitle() {
+        return videoTitle;
+    }   
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public String getVideoDescription() {
+        return videoDescription;
+    }
+
+    public int getVideoDuration() {
+        return videoDuration;
+    }
+
+    public void setVideoTitle(String videoNewTitle) {
+        this.videoTitle = videoNewTitle;
+    }
+
+    public void setVideoUrl(String videoNewUrl) {    
+        this.videoUrl = videoNewUrl;
+    }
+
+    public void setVideoDescription(String videoNewDescription) {
+        this.videoDescription = videoNewDescription;
+    }   
+
+    public void setVideoDuration(int videoNewDuration) {
+        this.videoDuration = videoNewDuration;
+    }
+
+    public void setVideoCourse(Course videoNewCourse) {
+        this.course = videoNewCourse;
+    }       
+
+}
