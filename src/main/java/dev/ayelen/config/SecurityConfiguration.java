7a7defd464b7226fcd60a3618ec2d8d03430dccc
Ayuik <marcosayelen@gmail.com>
2025-04-15 00:07:16 +0200

    feat: corsconfirguration done

--- src/main/java/dev/ayelen/config/SecurityConfiguration.java
+++ src/main/java/dev/ayelen/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package dev.ayelen.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,10 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Value("${api-endpoint}")
+    private String apiEndpoint;   
+     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -20,8 +25,9 @@ public class SecurityConfiguration {
                 .formLogin(form -> form.disable())
                 .authorizeHttpRequests(
                         auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-                                .requestMatchers("/courses").permitAll()
-                                .requestMatchers("/categories").permitAll()
+                                .requestMatchers(apiEndpoint + "/courses/**").permitAll()
+                                .requestMatchers(apiEndpoint + "/categories/**").permitAll()
+                                .requestMatchers(apiEndpoint + "/videos/**").permitAll()
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
         http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()))
