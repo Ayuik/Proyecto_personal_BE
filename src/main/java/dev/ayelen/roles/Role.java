65d830330ae26cbb5f2c4f9a72ad3df89357cef1
Ayuik <marcosayelen@gmail.com>
2025-04-19 11:00:21 +0200

    feat: Role created

---
+++ src/main/java/dev/ayelen/roles/Role.java
@@ -0,0 +1,43 @@
+package dev.ayelen.roles;
+
+import java.util.List;
+
+import dev.ayelen.users.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @OneToMany(mappedBy="role")
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Role(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public List<User> getUsers(){
+        return users;
+    }
+
+}
